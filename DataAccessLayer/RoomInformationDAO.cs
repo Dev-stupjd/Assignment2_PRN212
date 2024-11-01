c369a8a6c04fec1222ed8713fc9d684a6e25ac68
Dev-stupjd <khoindqde180179@fpt.edu.vn>
2024-11-01 10:57:10 +0700

    first

---
+++ DataAccessLayer/RoomInformationDAO.cs
@@ -0,0 +1,57 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class RoomInformationDAO : SingletonBase<RoomInformationDAO>
+    {
+        private HmsDBContext _context;
+
+        public RoomInformationDAO()
+        {
+            _context = new HmsDBContext();
+        }
+
+        public IEnumerable<RoomInformation> GetAllRoomInformation()
+        {
+            return _context.RoomInformations.ToList();
+        }
+
+        public void AddRoom(RoomInformation roomInformation)
+        {
+            _context.RoomInformations.Add(roomInformation);
+            _context.SaveChanges();
+        }
+
+        public void DeleteRoom(RoomInformation roomInformation)
+        {
+            var existingRoom = _context.RoomInformations.Find(roomInformation.RoomID);
+            if (existingRoom != null)
+            {
+                _context.RoomInformations.Remove(existingRoom);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new ArgumentException("Room information not found.");
+            }
+        }
+
+
+        public void UpdateRoom(RoomInformation roomInformation)
+        {
+            _context.RoomInformations.Update(roomInformation);
+            _context.SaveChanges();
+        }
+
+        public RoomInformation? GetRoomById(int id)
+        {
+            RoomInformation? room = _context.RoomInformations.FirstOrDefault(r => r.RoomID == id);
+            return room;
+        }
+    }
+}
